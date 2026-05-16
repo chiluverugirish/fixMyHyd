085b8f615dc1d780278db4678346eba8ff40284e
chiluverugirish <girishchieveru@gmail.com>
2026-05-16 08:48:03 +0530

    added .env

--- app.py
+++ app.py
@@ -7,12 +7,20 @@ app = create_app()
 
 if __name__ == "__main__":
     os.makedirs("temp", exist_ok=True)
+
     port = int(os.environ.get("PORT", 5001))
-    is_dev = os.environ.get("FLASK_ENV") == "development"
+    is_dev = os.environ.get("FLASK_ENV", "production") == "development"
+
+    print("===================================")
+    print("FixMyHyd Flask Server Starting...")
+    print(f"Running on Port: {port}")
+    print(f"Development Mode: {is_dev}")
+    print("===================================")
+
     app.run(
-        debug=is_dev,
-        use_reloader=is_dev,
-        threaded=True,
         host="0.0.0.0",
         port=port,
+        debug=is_dev,
+        threaded=True,
+        use_reloader=False
     )
